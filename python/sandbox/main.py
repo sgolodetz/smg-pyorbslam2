9776e31338b56d968b46701eff57547814c40c71
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-09-13 23:46:49 +0100

    Minor tidying.

--- python/sandbox/main.py
+++ python/sandbox/main.py
@@ -3,7 +3,6 @@ import numpy as np
 import pyorbslam
 
 from pyorbslam import CVMat1d, CVMat1f, CVMat3b
-from typing import List
 
 
 def load_colour_image(filename: str) -> CVMat3b:
@@ -25,21 +24,20 @@ def main():
 
     voc_file: str = "C:/orbslam/Vocabulary/ORBvoc.txt"
     settings_file: str = "D:/datasets/kitti_raw/2011_09_26/2011_09_26_drive_0005_sync/orb_slam/settings.yaml"
-    colour_images: List[CVMat3b] = [load_colour_image(filename) for filename in [
-        "D:/datasets/kitti_raw/2011_09_26/2011_09_26_drive_0005_sync/orb_slam/frame-000000.color.png",
-        "D:/datasets/kitti_raw/2011_09_26/2011_09_26_drive_0005_sync/orb_slam/frame-000001.color.png",
-    ]]
-    depth_images: List[CVMat1f] = [load_depth_image(filename) for filename in [
-        "D:/datasets/kitti_raw/2011_09_26/2011_09_26_drive_0005_sync/orb_slam/frame-000000.depth.png",
-        "D:/datasets/kitti_raw/2011_09_26/2011_09_26_drive_0005_sync/orb_slam/frame-000001.depth.png",
-    ]]
-    # pyorbslam.f(im)
-    system: pyorbslam.System = pyorbslam.System(voc_file, settings_file, pyorbslam.RGBD, False)
+    use_viewer: bool = True
+    system: pyorbslam.System = pyorbslam.System(voc_file, settings_file, pyorbslam.RGBD, use_viewer)
 
     timestamp: float = 0.0
-    for colour_image, depth_image in zip(colour_images, depth_images):
+    for idx in range(100):
+        colour_image: CVMat3b = load_colour_image(
+            f"D:/datasets/kitti_raw/2011_09_26/2011_09_26_drive_0005_sync/orb_slam/frame-{idx:06d}.color.png"
+        )
+        depth_image: CVMat1f = load_depth_image(
+            f"D:/datasets/kitti_raw/2011_09_26/2011_09_26_drive_0005_sync/orb_slam/frame-{idx:06d}.depth.png"
+        )
         pose: CVMat1d = system.track_rgbd(colour_image, depth_image, timestamp)
         print(np.array(pose, copy=False))
+        cv2.waitKey(1000)
         timestamp += 0.1
 
 
