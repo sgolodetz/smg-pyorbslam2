c0901e09804ced4b8f14f716e047c5ef393ad85b
Stuart Golodetz <sgolodetz@gxstudios.net>
2020-09-26 21:18:23 +0100

    Start getting the packaging working.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import glob
+import os
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="pyorbslam",
+    version="0.0.1",
+    author="Stuart Golodetz",
+    author_email="sgolodetz@gmail.com",
+    description="Python bindings for ORB-SLAM",
+    long_description="",  #long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=["pyorbslam"],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
