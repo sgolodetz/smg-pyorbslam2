716894c4fde2912452dfd0d1f1f2ed3a4f4849dc
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-08-01 21:11:53 +0100

    Bind ORBVocabulary.

--- pyorbslam2.cpp
+++ pyorbslam2.cpp
@@ -12,7 +12,46 @@ using namespace ORB_SLAM2;
 
 PYBIND11_MODULE(pyorbslam2, m)
 {
+  // ENUMERATIONS
+
+  py::enum_<DBoW2::ScoringType>(m, "EScoringType")
+    .value("L1_NORM", DBoW2::L1_NORM)
+    .value("L2_NORM", DBoW2::L2_NORM)
+    .value("CHI_SQUARE", DBoW2::CHI_SQUARE)
+    .value("KL", DBoW2::KL)
+    .value("BHATTACHARYYA", DBoW2::BHATTACHARYYA)
+    .value("DOT_PRODUCT", DBoW2::DOT_PRODUCT)
+    .export_values()
+  ;
+
+  py::enum_<System::eSensor>(m, "ESensor")
+    .value("MONOCULAR", System::MONOCULAR)
+    .value("STEREO", System::STEREO)
+    .value("RGBD", System::RGBD)
+    .export_values()
+  ;
+
+  py::enum_<DBoW2::WeightingType>(m, "EWeightingType")
+    .value("TF_IDF", DBoW2::TF_IDF)
+    .value("TF", DBoW2::TF)
+    .value("IDF", DBoW2::IDF)
+    .value("BINARY", DBoW2::BINARY)
+    .export_values()
+  ;
+
   // CLASSES
+
+  py::class_<ORBVocabulary>(m, "ORBVocabulary")
+    .def(
+      py::init<int, int, DBoW2::WeightingType, DBoW2::ScoringType>(),
+      py::arg("k") = 10, py::arg("ell") = 5, py::arg("weighting") = DBoW2::TF_IDF, py::arg("scoring") = DBoW2::L1_NORM,
+      py::call_guard<py::gil_scoped_release>()
+    )
+    .def("load_from_binary_file", &ORBVocabulary::loadFromBinaryFile, py::call_guard<py::gil_scoped_release>())
+    .def("load_from_text_file", &ORBVocabulary::loadFromTextFile, py::call_guard<py::gil_scoped_release>())
+    .def("save_to_binary_file", &ORBVocabulary::saveToBinaryFile, py::call_guard<py::gil_scoped_release>())
+    .def("save_to_text_file", &ORBVocabulary::saveToTextFile, py::call_guard<py::gil_scoped_release>())
+  ;
 
   py::class_<System>(m, "System")
     .def(py::init<std::string, std::string, System::eSensor, bool>(), py::call_guard<py::gil_scoped_release>())
@@ -23,14 +62,5 @@ PYBIND11_MODULE(pyorbslam2, m)
     .def("track_rgbd", [](System& self, const cv::Mat3b& im, const cv::Mat1f& depthmap, float timestamp) -> cv::Mat1d {
       return self.TrackRGBD(im, depthmap, timestamp);
     }, py::call_guard<py::gil_scoped_release>())
-  ;
-
-  // ENUMERATIONS
-
-  py::enum_<System::eSensor>(m, "ESensor")
-    .value("MONOCULAR", System::MONOCULAR)
-    .value("STEREO", System::STEREO)
-    .value("RGBD", System::RGBD)
-    .export_values()
   ;
 }
--- pyorbslam2.pyi
+++ pyorbslam2.pyi
@@ -1,15 +1,17 @@
 from smg.pyopencv import CVMat1d, CVMat1f, CVMat3b
 
 
-# CLASSES
-
-class System:
-	def __init__(self, voc_file: str, settings_file: str, sensor: ESensor, use_viewer: bool): ...
-	def shutdown(self) -> None: ...
-	def track_monocular(self, im: CVMat3b, timestamp: float) -> CVMat1d: ...
-	def track_rgbd(self, im: CVMat3b, depthmap: CVMat1f, timestamp: float) -> CVMat1d: ...
-
 # ENUMERATIONS
+
+class EScoringType(int):
+	pass
+
+L1_NORM: EScoringType
+L2_NORM: EScoringType
+CHI_SQUARE: EScoringType
+KL: EScoringType
+BHATTACHARYYA: EScoringType
+DOT_PRODUCT: EScoringType
 
 class ESensor(int):
 	pass
@@ -17,3 +19,28 @@ class ESensor(int):
 MONOCULAR: ESensor
 STEREO: ESensor
 RGBD: ESensor
+
+class EWeightingType(int):
+	pass
+
+TF_IDF: EWeightingType
+TF: EWeightingType
+IDF: EWeightingType
+BINARY: EWeightingType
+
+# CLASSES
+
+class ORBVocabulary:
+	def __init__(
+		self, k: int = 10, ell: int = 5, weighting: EWeightingType = TF_IDF, scoring: EScoringType = L1_NORM
+	): ...
+	def load_from_binary_file(self, filename: str) -> bool: ...
+	def load_from_text_file(self, filename: str) -> bool: ...
+	def save_to_binary_file(self, filename: str) -> None: ...
+	def save_to_text_file(self, filename: str) -> None: ...
+
+class System:
+	def __init__(self, voc_file: str, settings_file: str, sensor: ESensor, use_viewer: bool): ...
+	def shutdown(self) -> None: ...
+	def track_monocular(self, im: CVMat3b, timestamp: float) -> CVMat1d: ...
+	def track_rgbd(self, im: CVMat3b, depthmap: CVMat1f, timestamp: float) -> CVMat1d: ...
