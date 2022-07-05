534e33c7a72abb3484b2c56795bed6f3eee6f5f0
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-07-05 12:54:01 +0100

    Use scikit-build.

--- cmake/LinkOpenCV.cmake
+++ cmake/LinkOpenCV.cmake
@@ -2,12 +2,13 @@
 # LinkOpenCV.cmake #
 ####################
 
-TARGET_LINK_LIBRARIES(${targetname} ${OpenCV_LIBS})
+TARGET_LINK_LIBRARIES(${targetname} PRIVATE ${OpenCV_LIBS})
 
-IF(MSVC_IDE)
-  FILE(GLOB RUNTIMELIBS "${OpenCV_DIR}/bin/Debug/*.dll" "${OpenCV_DIR}/bin/Release/*.dll")
+IF(WIN32)
+  STRING(REPLACE "\\" "/" _OpenCV_LIB_PATH "${_OpenCV_LIB_PATH}")
+  FILE(GLOB OpenCV_RUNTIMELIBS "${_OpenCV_LIB_PATH}/*.dll")
 ENDIF()
 
-FOREACH(RUNTIMELIB ${RUNTIMELIBS})
+FOREACH(RUNTIMELIB ${OpenCV_RUNTIMELIBS})
   ADD_CUSTOM_COMMAND(TARGET ${targetname} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different ${RUNTIMELIB} "$<TARGET_FILE_DIR:${targetname}>")
 ENDFOREACH()
